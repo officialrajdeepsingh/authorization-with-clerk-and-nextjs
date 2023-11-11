fb1cf5a518af8a791ab80da74d371070da93665a
Rajdeep Singh <radewala.2@gmail.com>
2023-11-11 21:32:15 +0530

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+import { Header } from '@/components/Header'
+import {shadesOfPurple } from '@clerk/themes';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +18,27 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: shadesOfPurple,
+        variables: { colorPrimary: 'blue' },
+        layout: {
+          socialButtonsPlacement: 'bottom',
+          socialButtonsVariant: 'iconButton',
+          termsPageUrl: '/term',
+          privacyPageUrl: "/privacy-policy",
+          helpPageUrl: "/contact",
+          logoLinkUrl: "/auth-logo.png",
+          logoPlacement: "inside"
+        }
+      }} >
+
+      <html data-new-gr-c-s-check-loaded={true} data-gr-ext-installed={true} lang="en">
+        <body className={inter.className}>
+          <Header />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
