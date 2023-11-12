893e423182d81663ebc4be08d51c9c403708c421
Rajdeep Singh <radewala.2@gmail.com>
2023-11-12 22:11:34 +0530

    about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+
+    return (
+        <div className="my-8 mx-auto prose lg:prose-xl">
+            <h1>About us </h1>
+            <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic distinctio asperiores similique blanditiis natus quasi at architecto eos repudiandae ducimus maxime quidem recusandae cumque neque, ad, beatae fuga corporis ab?</p>
+        </div>
+    )
+}
