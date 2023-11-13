aac9cd63307016280e9a2d452547320ea5bf83ca
Rajdeep Singh <radewala.2@gmail.com>
2023-11-13 21:12:52 +0530

    remove the unused code

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,9 @@
 import { currentUser } from '@clerk/nextjs';
-// import { auth } from '@clerk/nextjs';
-// import { useUser } from "@clerk/nextjs";
+
 
 export default async function Dashboard() {
 
-  // const user = auth();
   const user = await currentUser();
-
-
-  // if (!isLoaded || !isSignedIn) {
-  //   return (<main className="flex flex-col items-center justify-between p-24">
-  //     <h1> Login </h1>
-  //   </main>)
-  // }
-  // publicRoutes: ["/dashboard"]
-  // console.log("my get user : ", user)
 
   if (!user) return <div> Not logged in </div>;
 
